bcdd94f9c7148877990443c60a03e27f1db026ab
maggiehubbard2 <55963135+maggiehubbard2@users.noreply.github.com>
2022-11-14 16:12:33 -0500

    Interface designed & functionality added

---
+++ Inventory-Manager/Inventory-Manager/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Inventory_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.skuLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.qtyLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.skuBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.qtyBox = new System.Windows.Forms.TextBox();
+            this.priceBox = new System.Windows.Forms.TextBox();
+            this.newBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.inventoryGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(251, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(481, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Inventory Manager Pro";
+            // 
+            // skuLabel
+            // 
+            this.skuLabel.AutoSize = true;
+            this.skuLabel.Location = new System.Drawing.Point(80, 90);
+            this.skuLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.skuLabel.Name = "skuLabel";
+            this.skuLabel.Size = new System.Drawing.Size(44, 17);
+            this.skuLabel.TabIndex = 1;
+            this.skuLabel.Text = "SKU#";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Location = new System.Drawing.Point(301, 90);
+            this.nameLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(75, 17);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "Item Name";
+            // 
+            // qtyLabel
+            // 
+            this.qtyLabel.AutoSize = true;
+            this.qtyLabel.Location = new System.Drawing.Point(580, 90);
+            this.qtyLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.qtyLabel.Name = "qtyLabel";
+            this.qtyLabel.Size = new System.Drawing.Size(61, 17);
+            this.qtyLabel.TabIndex = 3;
+            this.qtyLabel.Text = "Quantity";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(801, 90);
+            this.priceLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(40, 17);
+            this.priceLabel.TabIndex = 4;
+            this.priceLabel.Text = "Price";
+            // 
+            // skuBox
+            // 
+            this.skuBox.Location = new System.Drawing.Point(84, 110);
+            this.skuBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.skuBox.Name = "skuBox";
+            this.skuBox.Size = new System.Drawing.Size(187, 22);
+            this.skuBox.TabIndex = 5;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(305, 110);
+            this.nameBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(256, 22);
+            this.nameBox.TabIndex = 6;
+            // 
+            // qtyBox
+            // 
+            this.qtyBox.Location = new System.Drawing.Point(584, 110);
+            this.qtyBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.qtyBox.Name = "qtyBox";
+            this.qtyBox.Size = new System.Drawing.Size(199, 22);
+            this.qtyBox.TabIndex = 7;
+            // 
+            // priceBox
+            // 
+            this.priceBox.Location = new System.Drawing.Point(805, 110);
+            this.priceBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.priceBox.Name = "priceBox";
+            this.priceBox.Size = new System.Drawing.Size(123, 22);
+            this.priceBox.TabIndex = 8;
+            // 
+            // newBtn
+            // 
+            this.newBtn.Location = new System.Drawing.Point(84, 164);
+            this.newBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.newBtn.Name = "newBtn";
+            this.newBtn.Size = new System.Drawing.Size(255, 49);
+            this.newBtn.TabIndex = 9;
+            this.newBtn.Text = "New";
+            this.newBtn.UseVisualStyleBackColor = true;
+            this.newBtn.Click += new System.EventHandler(this.newBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(400, 164);
+            this.saveBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(241, 49);
+            this.saveBtn.TabIndex = 10;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.BackColor = System.Drawing.Color.IndianRed;
+            this.deleteBtn.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.deleteBtn.Location = new System.Drawing.Point(689, 164);
+            this.deleteBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(240, 49);
+            this.deleteBtn.TabIndex = 11;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = false;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // inventoryGridView
+            // 
+            this.inventoryGridView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.inventoryGridView.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.inventoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGridView.Location = new System.Drawing.Point(84, 249);
+            this.inventoryGridView.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.inventoryGridView.Name = "inventoryGridView";
+            this.inventoryGridView.Size = new System.Drawing.Size(845, 290);
+            this.inventoryGridView.TabIndex = 12;
+            this.inventoryGridView.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.inventoryGridView_CellDoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.inventoryGridView);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.newBtn);
+            this.Controls.Add(this.priceBox);
+            this.Controls.Add(this.qtyBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.skuBox);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.qtyLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.skuLabel);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Inventory Manager Pro 1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label skuLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label qtyLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.TextBox skuBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox qtyBox;
+        private System.Windows.Forms.TextBox priceBox;
+        private System.Windows.Forms.Button newBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.DataGridView inventoryGridView;
+    }
+}
+
---
+++ Inventory-Manager/Inventory-Manager/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Inventory_Manager
+{
+
+    //public class Item
+    //{   
+    //    public int qty { get; set; }
+    //    public int price { get; set; }
+    //    public int name { get; set; }
+    //    public int sku { get; set; }
+    //}
+
+    public partial class Form1 : Form
+    {
+
+        
+        DataTable inventory = new DataTable();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            char[] strOne, strTwo = new char[5];
+            //add columns to out datatable
+            inventory.Columns.Add("SKU");
+            inventory.Columns.Add("Name");
+            inventory.Columns.Add("Price");
+            inventory.Columns.Add("Quantity");
+
+            //connect gridview to our datatable
+            inventoryGridView.DataSource = inventory;
+
+        }
+
+        private void newBtn_Click(object sender, EventArgs e)
+        {
+            //clear all text boxes
+            skuBox.Text = "";
+            nameBox.Text = "";
+            qtyBox.Text = "";
+            priceBox.Text = "";
+
+
+
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            //save text box entries to variables
+            string sku = skuBox.Text;
+            string name = nameBox.Text;
+            string qty = qtyBox.Text;
+            string price = priceBox.Text;
+
+            //add values to datatable
+            inventory.Rows.Add(sku, name, price, qty);
+            //clears fields after saving
+            newBtn_Click(sender, e); 
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                inventory.Rows[inventoryGridView.CurrentCell.RowIndex].Delete();
+
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error: " + err);
+                throw;
+            }
+            //deleted selected row item
+            inventory.Rows[inventoryGridView.CurrentCell.RowIndex].Delete();
+        }
+
+        private void inventoryGridView_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                skuBox.Text = inventory.Rows[inventoryGridView.CurrentCell.RowIndex].ItemArray[0].ToString();
+                nameBox.Text = inventory.Rows[inventoryGridView.CurrentCell.RowIndex].ItemArray[1].ToString();
+                qtyBox.Text = inventory.Rows[inventoryGridView.CurrentCell.RowIndex].ItemArray[2].ToString();
+                priceBox.Text = inventory.Rows[inventoryGridView.CurrentCell.RowIndex].ItemArray[3].ToString();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
